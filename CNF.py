b219088fef9566413daa7d22d53d6c29006bfc6c
raphaelseegmuller <46761917+raphaelseegmuller@users.noreply.github.com>
2020-10-28 11:43:14 +0100

    Update CNF.py

--- CNF.py
+++ CNF.py
@@ -8,6 +8,20 @@ class CNF():
     def __init__(self, clauses):
         #clauses est une liste de liste
         self.clauses = clauses
+        
+    @property
+    def Lclauses(self):
+        return self.__Lcauses
+
+    @Lclauses.setter
+    def Lclauses(self, L):
+        if len(L) != 0:
+            test = True
+            for cl in L:
+                if type(cl) != 'clause':
+                    test = False
+            if test == True:
+                self.Lclauses = L
 
     def __len__(self):
         #donne le nombre de clauses
