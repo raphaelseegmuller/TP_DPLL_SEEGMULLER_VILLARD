f4911502c2088c0db93980040529ca71ffc4ef37
noeline-vld <73580817+noeline-vld@users.noreply.github.com>
2020-10-28 16:46:20 +0100

    Add files via upload

--- clause.py
+++ clause.py
@@ -2,11 +2,28 @@ __Author__ = "noeline"
 __Filename__ = "clause.py"
 __Creationdate__ = "23/10/20"
 
+from litteral import *
 
 class clause():
     def __init__(self, litteraux):
-        #litteraux est une miste de littéraux
+        #litteraux est une liste de littéraux
         self.litteraux = litteraux
+
+    @property
+    def litteraux(self):
+        return self.__litteraux
+
+    @litteraux.setter
+    def litteraux(self, x):
+        if x == []:
+            self.__litteraux = x
+        elif isinstance(x, list):
+            i = 0
+            for elt in x:
+                if not(isinstance(elt, litteral)):
+                    i += 1
+            if i == 0:
+                self.__litteraux = x
 
     def isEmpty(self):
         if len(self.litteraux) > 0:
@@ -17,4 +34,74 @@ class clause():
         if len(self.litteraux) == 1:
             return True
         return False
+
+    def prop(self):
+        #retourne la liste des variables propositionnelles d'une clause
+        liste = []
+        for elt in self.litteraux:
+            if elt.var() not in liste:
+                liste += [elt.var()]
+        return liste
+
+    def liste(self):
+        #retourne la liste des littéraux
+        liste = []
+        for elt in self.litteraux:
+            if elt == litteral(True):
+                liste += [True]
+            elif elt == litteral(False):
+                liste += [False]
+            else:
+                liste += [str(elt)]
+        return liste
+
+
+    def isThereLitteralPur(self):
+        #renvoie le premier litteral pur de la liste
+        liste = []
+        for elt in self.litteraux:
+            if elt not in liste and elt.oppose() not in liste:
+                liste += [str(elt)] # importance de mettre str(elt) et pas juste elt pck si on a litt1 = litteral('p')
+                                    # et litt7 = litteral('p') alors on aura 2 fois p dans liste (mais en vrai c'est
+                                    # pas très grave
+            elif elt.oppose() in liste:
+                liste.remove(str(elt.oppose()))
+        if len(liste) > 0:
+            return liste[0]
+        return None
+
+    def __str__(self):
+        #if self.isEmpty():
+        #    return str()
+        chaine = str(self.litteraux[0])
+        for elt in self.litteraux[1:]:
+            chaine += ' ou ' + str(elt)
+        return chaine
+
+    def addLitteral(self, litt: 'litteral'):
+        self.litteraux = self.litteraux + [litt]
+
+    def removeLitteral(self, litt: 'litteral'):
+        liste = []
+        for elt in self.litteraux:
+            if litt != elt:
+                liste += [elt]
+        return clause(liste)
+
+    def simplifie(self, prop: 'litteral', val: 'Bool'):
+        #la variable propositionnelle prend la valeur val
+        #supprime aussi les doublons
+        #ATTENTION prop doit etre positif
+        ans = clause([])
+        for elt in self.litteraux:
+            if elt.var() == prop.var():
+                ans.addLitteral(elt.simplifie(val))
+            elif elt.litt not in ans.liste():
+                ans.addLitteral(elt)
+        if True in ans.liste():
+            return clause([litteral(True)])
+        elif False in ans.liste():
+            return ans.removeLitteral(False)
+        else:
+            return ans
 
--- litteral.py
+++ litteral.py
@@ -2,8 +2,10 @@ __Author__ = "noeline"
 __Filename__ = "litteral.py.py"
 __Creationdate__ = "28/10/20"
 
-class litteral():
-    def __init__(self, litt: 'str'):
+import typing
+
+class litteral:
+    def __init__(self, litt):
         self.litt = litt
 
     @property
@@ -13,15 +15,50 @@ class litteral():
     @litt.setter
     def litt(self, x):
         #il n'y a pas de restriction sur les littérals, ca peut etre un prénom par exemple
-        self.__litt = x
+        if isinstance(x, str) or isinstance(x, bool): # les litteraux True et False sont acceptés
+            self.__litt = x
 
     def signe(self):
         #True si positif, False si négatif
-        if self.litt[0] == '-':
+        if isinstance(self.litt, bool):
+            return None
+        elif self.litt[0] == '-':
             return False
         return True
 
     def var(self):
-        if self.signe():
+        if self.signe() == True:
             return self.litt
-        return self.litt[1:]
+        elif self.signe() == False:
+            return self.litt[1:]
+        else: #self.litt == True ou == False
+            return self.litt
+
+
+    def oppose(self):
+        if self.litt == True: #importance du == true pck self.litt peut etre un str et "if str" rentre toujours dans le if
+            return litteral(False)
+        elif self.litt == False:
+            return litteral(True)
+        elif self.signe():
+            return '-' + self.var()
+        else:
+            return self.var()
+
+    def __str__(self):
+        # ATTENTION : il faut que le non reste un - pour la fonction isThereLittPur
+        return str(self.litt)
+
+    def simplifie(self, val: 'Bool'):
+        #met à val la valeure de la variable propositionnelle
+        if self.signe() == True:
+            return litteral(val)
+        elif self.signe() == False:
+            return litteral(val).oppose()
+        else: #self est un booléen --> il se simplifie par lui même
+            return self
+
+    def __eq__(self, litt2: 'litteral'):
+        if str(self) == str(litt2):
+            return True
+        return False
