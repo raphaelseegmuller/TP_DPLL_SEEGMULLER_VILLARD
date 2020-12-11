217d6845fc6c0396e74b94d44f300c608aef308c
noelinevld <75700068+noelinevld@users.noreply.github.com>
2020-12-11 10:29:36 +0100

    Add files via upload

---
+++ testfonctions.py
@@ -0,0 +1,21 @@
+__Author__ = "noeline"
+__Filename__ = "testfonctions.py"
+__Creationdate__ = "13/11/20"
+
+from fonctions import *
+
+n = 5
+
+print(dames(n))
+print('ans DPLL: ', dames(n).DPLL(ans=3, choice=None, Time=True))
+
+
+# "first_satisfy"
+# "first_fail"
+
+
+# si ans = 0 --> la méthode renvoie si la CNF est satisfaisable ou non (Booléen)
+# si ans = 1 --> la méthode renvoie une valuation si la CNF est satisfaisable, None sinon
+# si ans = 2 --> la méthode renvoie toutes les valuations de la CNF sous forme de liste (liste de liste)
+# si ans = 3 --> la méthode renvoie le nombre de valuations (entier)
+# si ans = 4 --> la méthode renvoie le nombre de noeuds de l'arbre (entier)
